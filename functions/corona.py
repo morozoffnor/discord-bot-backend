4d1880349f90f0a6e84424ec2df447bbd18ab952
Igor Morozov <morozofffmia@gmail.com>
2020-05-19 23:44:44 +0300

    corona update

--- functions/corona.py
+++ functions/corona.py
@@ -1,6 +1,7 @@
 import os
 import json
 import datetime
+import asyncio
 
 
 def currentCorona():
@@ -11,19 +12,37 @@ def currentCorona():
 
 def newCorona(coronaJson):
     ts = datetime.datetime.now().timestamp()
+
     with open('./data/corona.json', encoding="utf-8") as json_data_file:
         currentCoronaJSON = json.load(json_data_file)
-    then = currentCoronaJSON['timestamp']
+
+    lastTimestamp = currentCoronaJSON['timestamp']
     coronaObj = json.loads(coronaJson)
+    coronaObj['name'] = getNewCoronaName(coronaObj['name'])
+    # назначение полученных данных
     currentCoronaJSON['name'] = coronaObj['name']
     currentCoronaJSON['ownerId'] = coronaObj['ownerId']
     currentCoronaJSON['timestamp'] = ts
+
+    currentCoronaJSON['old']['name'] = deleteCoronaName(
+        currentCoronaJSON['old']['name'])
+
     file_object = open('./data/corona.json', 'w')
     json.dump(currentCoronaJSON, file_object)
 
     now = datetime.datetime.now().timestamp()
-    duration = now - then
+    duration = now - lastTimestamp
 
     currentCoronaJSON['string'] = "korona uje {}".format(duration)
 
     return currentCoronaJSON
+
+
+def deleteCoronaName(name):
+    newName = name.replace("♔", "")
+    return newName
+
+
+def getNewCoronaName(name):
+    newName = "♔ " + name
+    return newName
